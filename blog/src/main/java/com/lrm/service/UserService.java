3b46c6db2d223df272e03a5dc4882270eca1f60e
One <5576648+One_dayday@user.noreply.gitee.com>
2020-11-10 11:20:05 +0800

    拦截器

---
+++ blog/src/main/java/com/lrm/service/UserService.java
@@ -0,0 +1,18 @@
+package com.lrm.service;
+
+import com.lrm.dao.UserRepository;
+import com.lrm.po.User;
+import com.lrm.util.MD5Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User checkUser(String username,String password){
+        return  userRepository.findByUsernameAndPassword(username, MD5Utils.code(password));
+    }
+}
